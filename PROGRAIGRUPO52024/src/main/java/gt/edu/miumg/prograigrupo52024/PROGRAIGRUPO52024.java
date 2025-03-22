3942dda208ce996abb65f39ba62d779aec286a8d
loren <loren@Cris-Inspiron>
2025-03-22 03:04:28 -0600

    Inital commit

---
+++ PROGRAIGRUPO52024/src/main/java/gt/edu/miumg/prograigrupo52024/PROGRAIGRUPO52024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gt.edu.miumg.prograigrupo52024;
+
+/**
+ *
+ * @author loren
+ */
+public class PROGRAIGRUPO52024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
