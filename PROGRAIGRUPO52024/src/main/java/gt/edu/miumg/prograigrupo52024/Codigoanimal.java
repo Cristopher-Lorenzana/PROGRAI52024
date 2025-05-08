93eea66c38684a831bb54b117ff51e60b55d3b72
loren <loren@Cris-Inspiron>
2025-05-08 03:30:39 -0600

    Codigo que necesito

---
+++ PROGRAIGRUPO52024/src/main/java/gt/edu/miumg/prograigrupo52024/Codigoanimal.java
@@ -0,0 +1,20 @@
+
+package gt.edu.miumg.prograigrupo52024;
+
+
+public class Codigoanimal {
+    
+    @Override
+    public String toString() {
+        return "Animal{" + 
+               "nombre='" + nombre + '\'' + 
+               ", edad=" + edad + 
+               ", peso=" + peso + " kg" + 
+               ", dieta='" + dieta + '\'' + 
+               '}';
+    
+    
+    
+    
+    
+}
